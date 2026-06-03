a96ea9f097c2d68d34869d5d16968ad189c42682
flxnsn <flxnsn@web.de>
2026-06-03 18:14:41 +0200

    Build version to test deployment

--- frontend/src/components/PageHome.jsx
+++ frontend/src/components/PageHome.jsx
@@ -1,6 +1,8 @@
 export default function PageHome({ onNavigate }) {
   return (
     <>
+    <p>"I am currently under construction</p>
+    <p>Thank you for your patience" ~ES</p>
       <div className="home-buttons">
         <button className="home-btn btn-it"     onClick={() => onNavigate('it')}>IT</button>
         <button className="home-btn btn-design" onClick={() => onNavigate('design')}>Design</button>
